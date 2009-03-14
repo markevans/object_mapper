a816e861092a4972922397c0057cd3d79077bd71
Mark Evans <mark@new-bamboo.co.uk>
2009-03-14 17:18:27 +0000

    Added to_setter and related methods to method call class

--- lib/object_mapper/method_call.rb
+++ lib/object_mapper/method_call.rb
@@ -11,6 +11,33 @@ module ObjectMapper
     def call_on(obj)
       obj.send(method, *args)
     end
-  
+    
+    def to_a
+      [method, *args]
+    end
+    
+    def setter?
+      !!(method.to_s =~ /=$/)
+    end
+    
+    def getter?
+      !setter?
+    end
+    
+    def to_setter(value)
+      new_method_call = self.dup
+      if setter?
+        new_method_call.args[-1] = value
+      else
+        new_method_call.args << value
+        new_method_call.method = "#{new_method_call.method}=".to_sym
+      end
+      new_method_call
+    end
+    
+    protected
+    
+    attr_writer :method
+    
   end
 end
--- spec/method_call_spec.rb
+++ spec/method_call_spec.rb
@@ -31,4 +31,52 @@ describe MethodCall do
     end
   end
   
+  describe "to array" do
+    it "should return the method and args as an array" do
+      MethodCall.new(:egg, 'cheese', 5).to_a.should == [:egg, 'cheese', 5]
+    end
+  end
+  
+  describe "setter?" do
+    it "should return true for a setter" do
+      MethodCall.new(:doobie=, '4').setter?.should be_true
+      MethodCall.new(:[]=, 2, 3).setter?.should be_true
+    end
+    it "should return false for a getter" do
+      MethodCall.new(:doobie, '4').setter?.should be_false
+      MethodCall.new(:[], 2, 3).setter?.should be_false
+    end
+  end
+  
+  describe "getter?" do
+    it "should be false if setter is true" do
+      @method_call.should_receive(:setter?).and_return true
+      @method_call.getter?.should be_false
+    end
+    it "should be true if setter is false" do
+      @method_call.should_receive(:setter?).and_return false
+      @method_call.getter?.should be_true
+    end
+  end
+  
+  describe "making methods into setters" do
+
+    it "should turn [] into a setter" do
+      MethodCall.new(:[], 3).to_setter('hello').to_a.should == [:[]=, 3, 'hello']
+    end
+
+    it "should leave []= as it is but assign new value" do
+      MethodCall.new(:[]=, 3, 5).to_setter('hello').to_a.should == [:[]=, 3, 'hello']
+    end
+
+    it "should turn an arbitrary method call into a setter by adding the value at the end of the args" do
+      MethodCall.new(:hello, 'there').to_setter(65).to_a.should == [:hello=, 'there', 65]
+    end
+
+    it "should not change an arbitrary method which is already a setter but assign a new value" do
+      MethodCall.new(:hello=, 'there').to_setter(65).to_a.should == [:hello=, 65]
+    end
+
+  end
+  
 end
